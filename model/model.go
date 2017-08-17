ca25cc0299537016f1498defc36491c889f5e82d
Sergey <SergeyVorobevWork@gmail.com>
2017-08-17 20:34:49 +0300

    initial commit

--- model/model.go
+++ model/model.go
@@ -1,20 +1,24 @@
 package model
 
+import (
+	"chessServer/utility"
+	"debug/plan9obj"
+)
+
+// TODO add classes Straight figure with collisions and figure with collisions in place only
 //	interface for all figures, just checking if figure can go somewhere/attack a field
 type StepMaker interface{
-	CheckStepAvailable(point Point)(bool)	//	check if figure can go to the following field
-	CheckAttackAvailable(point Point)(bool)	//	check if figure can attack the following field
-	StepsAvailable()([]Point, bool)	//	list all available fields to go
-	AttacksAvailable()([]Point, bool)	//	list all available fields to attack
+	CheckStepAvailable(point utility.Point)(bool)	//	check if figure can go to the following field
+	CheckAttackAvailable(point utility.Point)(bool)	//	check if figure can attack the following field
+	StepsAvailable()([]utility.Point)	//	list all available fields to go
+	AttacksAvailable()([]utility.Point)	//	list all available fields to attack
+	CheckForCollision(destination,obstacle utility.Point)(bool)	//	check if we can collide with other figure while doing step
 }
 
-type Point struct{
-	X uint
-	Y uint
-}
+
 
 type Figure struct{	//	parent class for all figures(all figures inherits Figure and implement StepMaker)
-	Point	//	figure coordinates
+	utility.Point	//	figure coordinates
 	Colour_ Colour
 }
 
@@ -25,6 +29,43 @@ type King struct{
 	Figure
 }
 
+func (k King)StepsAvailable()(Buffer []utility.Point){
+	Buffer = make([]utility.Point, 8)
+	for i:=k.X-1;i <= k.X+1;i++{
+		for j:=k.Y-1;j <= k.Y+1;j++{
+			temp:=utility.Point{i,j}
+			if !temp.Equal(k.Point){
+				if temp.CheckFieldBoundaries(){
+					Buffer = append(Buffer, temp)
+				}
+			}
+		}
+	}
+	return Buffer
+}
+
+func (k King)AttacksAvailable()(Buffer []utility.Point){
+	Buffer = k.StepsAvailable()
+	return Buffer
+}
+
+func (k King)CheckStepAvailable(point utility.Point)(bool){
+	stepsAvailable:=k.StepsAvailable()
+	for _,element:=range stepsAvailable{
+		if element.Equal(point){
+			return true
+		}
+	}
+	return false
+}
+
+func (k King)CheckAttackAvailable(point utility.Point)(bool){
+	return k.CheckStepAvailable(point)
+}
+
+func (k King)CheckForCollision(destination, obstacle utility.Point)(bool){
+	return destination.Equal(obstacle)
+}
 type Queen struct{
 	Figure
 }
@@ -33,11 +74,87 @@ type Bishop struct{
 	Figure	//	slon
 }
 
+func(b Bishop)StepsAvailable()(Buffer []utility.Point){
+	Buffer = make([]utility.Point, 16)
+	for i:=0;i<8;i++{
+		tmp1,tmp2:=utility.Point{b.X,i},utility.Point{i,b.Y}
+		if !tmp1.Equal(b.Point){
+			Buffer = append(Buffer,tmp1)
+		}
+		if !tmp2.Equal(b.Point){
+			Buffer = append(Buffer,tmp2)
+		}
+	}
+	return Buffer
+}
+
+func (b Bishop)AttacksAvailable()(Buffer []utility.Point){
+	return b.StepsAvailable()
+}
+
+func (b Bishop)CheckStepAvailable(point utility.Point)(bool){
+	for _,element:=range b.StepsAvailable(){
+		if b.Point.Equal(element){
+			return true
+		}
+	}
+	return false
+}
+
+func (b Bishop)CheckAttackAvailable(point utility.Point)(bool){
+	return b.CheckStepAvailable(point)
+}
+
+func (b Bishop)CheckForCollision(destination, obstacle utility.Point)(bool){
+	if b.CheckStepAvailable(destination){
+		way:=utility.Line{b.Point, destination}
+		return way.Intersect(obstacle)
+	}else{
+		return false
+	}
+}
+
 type Knight struct{
 	Figure	//	kon
 }
 
-type R
+func(k Knight)StepsAvailable()(Buffer []utility.Point){
+	storage:=[]utility.Point{Point{},Point{}}
+	Buffer = make([]utility.Point,8)
+	return Buffer
+}
+
+func (k Knight)AttacksAvailable()(Buffer []utility.Point){
+	return b.StepsAvailable()
+}
+
+func (k Knight)CheckStepAvailable(point utility.Point)(bool){
+	for _,element:=range b.StepsAvailable(){
+		if b.Point.Equal(element){
+			return true
+		}
+	}
+	return false
+}
+
+func (k Knight)CheckAttackAvailable(point utility.Point)(bool){
+	return b.CheckStepAvailable(point)
+}
+
+func (k Knight)CheckForCollision(destination, obstacle utility.Point)(bool){
+	if b.CheckStepAvailable(destination){
+		way:=utility.Line{b.Point, destination}
+		return way.Intersect(obstacle)
+	}else{
+		return false
+	}
+}
+
+
+type Rook struct{
+	Figure	//	ladya
+}
+
 
 
 type Colour uint8
--- utility/utility.go
+++ utility/utility.go
@@ -1,1 +1,61 @@
 package utility
+
+import "math"
+
+//package containing utility functions
+type Point struct{
+	X int
+	Y int
+}
+
+func (p Point) CheckFieldBoundaries() bool{
+	return p.X >=0 && p.X < 8 && p.Y >=0 && p.Y <8
+}
+
+func (p Point)Equal(compared Point)bool{
+	return p.X == compared.X &&p.Y == compared.Y
+}
+
+type Line struct{
+	begin Point
+	end   Point
+}
+
+func (l Line)abs()float64{
+	p:=Point{l.end.X - l.begin.X, l.end.Y - l.begin.Y}
+	return math.Sqrt(float64(p.X*p.X+p.Y*p.Y))
+}
+
+func (l Line)tan()(float64, bool){
+	p:=Point{l.end.X - l.begin.X, l.end.Y - l.begin.Y}
+	if p.X !=0{
+		return  float64(p.Y)/float64(p.X),true
+	}else{
+		return 0,false
+	}
+}
+
+func (l Line) Intersect(point Point)bool{
+	if l.end.X == l.begin.X{
+		if l.end.X == point.X{
+			vec:=Line{l.begin,point}
+			if sameSign(l.end.Y - l.begin.Y,point.Y-l.begin.Y) && l.abs() > vec.abs(){
+				return true
+			}else{
+				return false
+			}
+		}else{
+			return false
+		}
+	}else{
+		vec:=Line{l.begin,point}
+		return l.abs() > vec.abs() && l.tan() == vec.tan()
+	}
+}
+
+func sameSign(a,b int)bool{
+	return (a > 0) == (b > 0)
+}
+
+
+
